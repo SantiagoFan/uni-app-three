31e656e515f20765c2fe8b34aed3e7a8751528ca
limengnan <limengnan@nmgjoin.onaliyun.com>
2021-01-11 14:48:50 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,22 +1,25 @@
-import Vue from 'vue'
-import App from './App'
-import store from '@/store'
-import uView from 'uview-ui'
+import Vue from "vue";
+import App from "./App";
+import store from "@/store";
+import uView from "uview-ui";
 
-import mixin from '@/utils/mixin';
+import mixin from "@/utils/mixin";
+// import Router from "uni-simple-router";
+// console.log(Router);
+// Vue.use(Router);
 
-Vue.prototype.$bus = new Vue() // event Bus 用于兄弟组件的通信。
+Vue.prototype.$bus = new Vue(); // event Bus 用于兄弟组件的通信。
 
-App.mpType = 'app'
+App.mpType = "app";
 
-Vue.use(uView)
+Vue.use(uView);
 
-Vue.mixin(mixin)
+Vue.mixin(mixin);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 const app = new Vue({
   ...App,
-  store
-})
-app.$mount()
+  store,
+});
+app.$mount();
---
+++ src/router/modules/home.js
@@ -0,0 +1,18 @@
+// router/modules/home.js
+const home = [
+  {
+    path: "/pages/index/index",
+    name: "index",
+    meta: {
+      title: "首页",
+    },
+  },
+  {
+    path: "/pages/patientManage/patientManage",
+    name: "patientManage",
+    meta: {
+      title: "问卷调查",
+    },
+  },
+];
+export default home;
