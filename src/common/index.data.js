0e6b9effff55c972eec39fb44f900fdc7cf82f68
limengnan <limengnan@nmgjoin.onaliyun.com>
2021-01-21 16:03:10 +0800

    1

--- src/common/index.data.js
+++ src/common/index.data.js
@@ -1,99 +1,124 @@
 export default {
   list1: [
     {
-      "title": "添加就诊人",
-      "image": require("@/static/image/indx-sta1.jpg"),
-      "url": "/pages/patientAdd/patientAdd",
-      "openType": "navigate"
-    }, {
-      "title": "排队候诊",
-      "image": require("@/static/image/indx-sta2.jpg"),
-      "url": "/pages/myLineUp/myLineUp",
-      "openType": "navigate"
-    }, {
-      "title": "报告查询",
-      "image": require("@/static/image/indx-sta3.jpg"),
-      "url": "/pages/checkReport/checkReport",
-      "openType": "navigate"
-    }, {
-      "title": "调查问卷",
-      "image": require("@/static/image/indx-sta4.jpg"),
-      "url": "/pages/survey/survey",
-      "openType": "navigate"
-    }, {
-      "title": "微网站",
-      "image": require("@/static/image/indx-sta5.jpg"),
-      "url": "/pages/hospital/hospital",
-      "openType": "switchTab"
-    }, {
-      "title": "挂号记录",
-      "image": require("@/static/image/indx-sta6.jpg"),
-      "url": "/pages/registerRecord/registerRecord",
-      "openType": "navigate"
-    }, {
-      "title": "缴费记录",
-      "image": require("@/static/image/indx-sta7.jpg"),
-      "url": "/pages/payRecord/payRecord",
-      "openType": "navigate"
-    }, {
-      "title": "报告下载",
-      "image": require("@/static/image/indx-sta8.jpg"),
-      "url": "/pages/checkReport/checkReport",
-      "openType": "navigate"
-    }, {
-      "title": "健康指导",
-      "image": require("@/static/image/indx-sta9.jpg"),
-      "url": "/pages/hospitalTrends/hospitalTrends",
-      "openType": "navigate"
-    }, {
-      "title": "诊间支付",
-      "image": require("@/static/image/indx-sta10.jpg"),
-      "url": "/pages/awaitPay/awaitPay",
-      "openType": "navigate"
-    }
+      title: '添加就诊人',
+      image: require('@/static/image/indx-sta1.jpg'),
+      name: 'patientAdd',
+      openType: 'push',
+    },
+    {
+      title: '排队候诊',
+      image: require('@/static/image/indx-sta2.jpg'),
+      name: 'myLineUp',
+      openType: 'push',
+    },
+    {
+      title: '报告查询',
+      image: require('@/static/image/indx-sta3.jpg'),
+      name: 'checkReport',
+      openType: 'push',
+    },
+    {
+      title: '调查问卷',
+      image: require('@/static/image/indx-sta4.jpg'),
+      name: 'survey',
+      openType: 'push',
+    },
+    {
+      title: '微网站',
+      image: require('@/static/image/indx-sta5.jpg'),
+      name: 'hospital',
+      openType: 'pushTab',
+    },
+    {
+      title: '挂号记录',
+      image: require('@/static/image/indx-sta6.jpg'),
+      name: 'registerRecord',
+      openType: 'push',
+    },
+    {
+      title: '缴费记录',
+      image: require('@/static/image/indx-sta7.jpg'),
+      name: 'payRecord',
+      openType: 'push',
+    },
+    {
+      title: '报告下载',
+      image: require('@/static/image/indx-sta8.jpg'),
+      name: 'checkReport',
+      openType: 'push',
+    },
+    {
+      title: '健康指导',
+      image: require('@/static/image/indx-sta9.jpg'),
+      name: 'hospitalTrends',
+      openType: 'push',
+    },
+    {
+      title: '诊间支付',
+      image: require('@/static/image/indx-sta10.jpg'),
+      name: 'awaitPay',
+      openType: 'push',
+    },
   ],
   list2: [
     {
-      "title": "住院日清单",
-      "image": require("@/static/image/indx-sta11.jpg"),
-      "url": "/pages/myResidentBill/myResidentBill",
-      "openType": "navigate"
-    }, {
-      "title": "住院缴费",
-      "image": require("@/static/image/indx-sta12.jpg"),
-      "url": "/pages/awaitPayPatient/awaitPayPatient",
-      "openType": "navigate"
-    }, {
-      "title": "住院人信息",
-      "image": require("@/static/image/indx-sta13.jpg"),
-      "url": "/pages/myResidents/myResidents",
-      "openType": "navigate"
-    }
+      title: '住院日清单',
+      image: require('@/static/image/indx-sta11.jpg'),
+      name: 'myResidentBill',
+      openType: 'push',
+    },
+    {
+      title: '住院缴费',
+      image: require('@/static/image/indx-sta12.jpg'),
+      name: 'awaitPayPatient',
+      openType: 'push',
+    },
+    {
+      title: '住院人信息',
+      image: require('@/static/image/indx-sta13.jpg'),
+      name: 'myResidents',
+      openType: 'push',
+    },
   ],
-  hospitalList1: [{
-    title: '全力以赴，坚决打赢这场战疫 ！',
-    date: '2020-07-18  09:00:00',
-    img: 'https://www.tanakadental-hino.net/wp-content/uploads/clinic_img002.jpg'
-  }],
-  hospitalList2: [{
-    title: '全力以赴，坚决打赢这场战疫 ！',
-    date: '2020-07-18  09:00:00',
-    img: 'https://img.zcool.cn/community/01343e5568ebbe000001271645b7ae.jpg@1280w_1l_2o_100sh.jpg'
-  },{
-    title: '呼市互联网发热咨询门诊市民手册',
-    date: '2020-07-18  09:00:00',
-    img: 'https://www.tanakadental-hino.net/wp-content/uploads/clinic_img002.jpg'
-  },{
-    title: '做实做细，坚决打赢疫情防控阻击战',
-    date: '2020-07-18  09:00:00',
-    img: 'https://tse1-mm.cn.bing.net/th/id/OIP.9rvexzEsyOmgP6Ud8161YAHaE8?pid=Api&rs=1'
-  },{
-    title: '今日再出征 期待早归来',
-    date: '2020-07-18  09:00:00',
-    img: 'https://tse2-mm.cn.bing.net/th/id/OIP.Hyimsf-e7Z_ip46hYYTjcwHaEK?pid=Api&rs=1'
-  },{
-    title: '让爱在这里传递',
-    date: '2020-07-18  09:00:00',
-    img: 'https://www.tanakadental-hino.net/wp-content/uploads/clinic_img002.jpg'
-  }]
+  hospitalList1: [
+    {
+      title: '全力以赴，坚决打赢这场战疫 ！',
+      date: '2020-07-18  09:00:00',
+      img:
+        'https://www.tanakadental-hino.net/wp-content/uploads/clinic_img002.jpg',
+    },
+  ],
+  hospitalList2: [
+    {
+      title: '全力以赴，坚决打赢这场战疫 ！',
+      date: '2020-07-18  09:00:00',
+      img:
+        'https://img.zcool.cn/community/01343e5568ebbe000001271645b7ae.jpg@1280w_1l_2o_100sh.jpg',
+    },
+    {
+      title: '呼市互联网发热咨询门诊市民手册',
+      date: '2020-07-18  09:00:00',
+      img:
+        'https://www.tanakadental-hino.net/wp-content/uploads/clinic_img002.jpg',
+    },
+    {
+      title: '做实做细，坚决打赢疫情防控阻击战',
+      date: '2020-07-18  09:00:00',
+      img:
+        'https://tse1-mm.cn.bing.net/th/id/OIP.9rvexzEsyOmgP6Ud8161YAHaE8?pid=Api&rs=1',
+    },
+    {
+      title: '今日再出征 期待早归来',
+      date: '2020-07-18  09:00:00',
+      img:
+        'https://tse2-mm.cn.bing.net/th/id/OIP.Hyimsf-e7Z_ip46hYYTjcwHaEK?pid=Api&rs=1',
+    },
+    {
+      title: '让爱在这里传递',
+      date: '2020-07-18  09:00:00',
+      img:
+        'https://www.tanakadental-hino.net/wp-content/uploads/clinic_img002.jpg',
+    },
+  ],
 }
