e7b2a380f545a4b7d41124d8b23db2812003daad
SanF <464884785@qq.com>
2021-01-28 18:31:46 +0800

    x

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -10,33 +10,7 @@
         ></dh-image>
       </view>
       <view class="index-wrap">
-        <view class="index-wrap__user">
-          <view class="index-wrap__user-info">
-            <view v-if="patientInfo">
-              <view class="title">
-                <view class="name">{{ getName(patientInfo.name) }}</view>
-                <view class="tag">电子就诊卡</view>
-                <view @click="changePatient" class="check">切换</view>
-              </view>
-              <view class="code"
-                >院内诊疗号：{{ patientInfo.patient_code }}</view
-              >
-            </view>
-            <view @click="addPatient" class="add_btn" v-else>
-              <view class="add_btn__icon">
-                <text class="iconfont icon-add-fill"></text>
-              </view>
-              <view class="add_btn__text">添加就诊卡</view>
-            </view>
-          </view>
-          <view class="index-wrap__user-pic">
-            <image
-              class="img"
-              mode="widthFix"
-              src="@/static/image/index_rw.png"
-            />
-          </view>
-        </view>
+        <PatientCard theme="card" :show-message="false"></PatientCard>
         <template v-if="patientInfo">
           <view class="index-wrap__art1">
             <router-link
@@ -160,36 +134,6 @@
         </view>
       </view>
     </u-popup>
-    <u-popup v-model="show" mode="bottom" :border-radius="20">
-      <view class="check-wrap">
-        <view class="check-wrap__title">切换就诊人</view>
-        <view class="check-wrap__con">
-          <view class="list" v-if="patientList.length > 0">
-            <view
-              :class="['item', item.is_default ? 'active' : '']"
-              @click="choicePatient(item.id)"
-              v-for="(item, index) in patientList"
-              :key="index"
-            >
-              <view class="info">
-                <view class="name">{{ item.name }}</view>
-                <view class="code">就诊卡：{{ item.patient_code }}</view>
-              </view>
-              <view class="radio">
-                <text class="iconfont icon-duihao"></text>
-              </view>
-            </view>
-          </view>
-          <view v-else class="nodata">
-            暂无更多就诊人
-          </view>
-        </view>
-        <view class="check-wrap__btn">
-          <view class="item" @click="addPatient">添加就诊人</view>
-          <view class="item" @click="managePatient">管理就诊人</view>
-        </view>
-      </view>
-    </u-popup>
     <auth></auth>
   </view>
 </template>
@@ -215,7 +159,6 @@ export default {
       list1: indexList.list1,
       list2: indexList.list2,
       visitCodeShow: false, // 就诊码
-      show: false, //  切换就诊人
       patientList: [],
     }
   },
@@ -247,38 +190,12 @@ export default {
       }
     },
     //就诊人
-    getPatientList() {
-      this.$http.post(this.API.PATIENT_LIST).then((res) => {
-        this.patientList = res.data
-      })
-    },
     handleTypeItem(index) {
       this.typeIndex = index
     },
     // 就诊码
     handleVisitCode() {
       this.visitCodeShow = true
-    },
-    changePatient() {
-      this.show = true
-      this.getPatientList()
-    },
-    choicePatient(id) {
-      this.$http
-        .post(this.API.CHANGE_DEFAULT_PATIENT, { id: id })
-        .then((res) => {
-          if (res.code == 20000) {
-            this.$store.commit('setPatientInfo', res.data)
-            // this.$store.dispatch("getDefaultPatient");
-            this.show = false
-          }
-        })
-    },
-    addPatient() {
-      this.$Router.push({ name: 'medicalCardLogin' })
-    },
-    managePatient() {
-      this.$Router.push({ name: 'patientAdd' })
     },
   },
 }
@@ -300,85 +217,6 @@ export default {
       margin-top: -80rpx;
       padding-top: 80rpx;
       border-radius: 35rpx 35rpx 0 0;
-      &__user {
-        display: flex;
-        align-items: center;
-        position: absolute;
-        top: -80rpx;
-        left: 50%;
-        transform: translateX(-50%);
-        width: 680rpx;
-        height: 160rpx;
-        padding: 0 25rpx;
-        border-radius: 20rpx;
-        box-shadow: 0 10rpx 12rpx rgba($color: #61b47c, $alpha: 0.2);
-        background: #ffffff url('@/static/image/box_bg.png') no-repeat 18rpx
-          18rpx;
-        background-size: 250rpx;
-        &-info {
-          color: #0ec698;
-          font-size: 24rpx;
-          .add_btn {
-            display: inline-flex;
-            align-items: center;
-            height: 82rpx;
-            padding: 0 30rpx;
-            color: #0ec698;
-            border: 1rpx solid #0ec698;
-            border-radius: 20rpx;
-            &__icon {
-              margin-right: 20rpx;
-              .iconfont {
-                font-size: 45rpx;
-              }
-            }
-            &__text {
-              font-size: 32rpx;
-            }
-          }
-          .title {
-            display: flex;
-            align-items: center;
-            margin-bottom: 20rpx;
-            .name {
-              max-width: 110rpx;
-              font-size: 36rpx;
-              font-weight: bold;
-              word-wrap: break-word;
-              @include textOverflow(1);
-            }
-            .tag {
-              height: 32rpx;
-              line-height: 32rpx;
-              font-size: 20rpx;
-              margin: 0 25rpx;
-              padding: 0 10rpx;
-              border: 1rpx solid #0ec698;
-              border-radius: 10rpx;
-            }
-            .check {
-              width: 92rpx;
-              height: 40rpx;
-              line-height: 40rpx;
-              text-align: center;
-              color: #ffffff;
-              background: #0fd1a1;
-              border-radius: 20rpx;
-            }
-          }
-        }
-        &-pic {
-          position: absolute;
-          bottom: 0;
-          right: 0;
-          width: 280rpx;
-          .img {
-            width: 100%;
-            height: auto;
-            display: block;
-          }
-        }
-      }
       &__art1 {
         display: flex;
         align-items: center;
@@ -510,80 +348,6 @@ export default {
         width: 325rpx;
         height: 325rpx;
         display: block;
-      }
-    }
-  }
-  //切换就诊人
-  // 弹出层
-  .check-wrap {
-    &__title {
-      height: 84rpx;
-      line-height: 84rpx;
-      text-align: center;
-      color: #333333;
-      font-size: 32rpx;
-      border-bottom: 1rpx solid #e3e3e3;
-    }
-    &__con {
-      .list {
-        .item {
-          display: flex;
-          align-items: center;
-          justify-content: space-between;
-          padding: 20rpx 40rpx;
-          .info {
-            font-size: 28rpx;
-            .name {
-              color: #333333;
-            }
-            .code {
-              color: #999999;
-              margin-top: 10rpx;
-            }
-          }
-          .radio {
-            width: 54rpx;
-            height: 54rpx;
-            line-height: 54rpx;
-            text-align: center;
-            color: #ffffff;
-            border-radius: 50%;
-            border: 1rpx solid;
-            border-color: #e3e3e3;
-            .icon {
-              display: none;
-            }
-          }
-          &.active {
-            .radio {
-              border-color: #0ec698;
-              background: #0ec698;
-              .icon {
-                display: block;
-              }
-            }
-          }
-        }
-      }
-      .nodata {
-        padding: 20rpx;
-        color: #999999;
-        text-align: center;
-      }
-    }
-    &__btn {
-      display: flex;
-      justify-content: space-between;
-      padding: 30rpx 70rpx;
-      .item {
-        width: 275rpx;
-        height: 64rpx;
-        line-height: 64rpx;
-        color: #ffffff;
-        text-align: center;
-        font-size: 28rpx;
-        border-radius: 8rpx;
-        background: #0ec698;
       }
     }
   }
