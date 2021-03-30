54f17423f12fe3e409f30f57a21d943907ad06f6
Administrator <san_fan@qq.com>
2021-03-30 20:24:06 +0800

    edit user_name

--- src/pages/medicalCard/medicalCardRegister.vue
+++ src/pages/medicalCard/medicalCardRegister.vue
@@ -213,19 +213,7 @@ export default {
                 this.formData.phone = r.data.mobile
                 this.formData.idcard = r.data.cert_no
               })
-              // 认证成功      // 调用自己的服务端接口，让服务端进行后端的授权认证，并且利用session，需要解决跨域问题
-              // my.request({
-              //   url: 'https://isv.com/auth', // 该url是您自己的服务地址，实现的功能是服务端拿到authcode去开放平台进行token验证        
-              //   data: {
-              //     authcode: res.authCode,
-              //   },
-              //   success: () => {
-              //     // 授权成功并且服务器端登录成功        
-              //   },
-              //   fail: () => {
-              //     // 根据自己的业务场景来进行错误处理
-              //   },
-              // });
+
             }
           },
           fail:_=>{
